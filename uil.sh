d9125ba93e3c386fc34996e4bd1921b6bb524cc5
Whiskerpillar <Mawwnw@gmail.com>
2026-01-23 13:37:36 -0500

    Update uil.sh

--- uil.sh
+++ uil.sh
@@ -2,9 +2,14 @@
 #Logging suite named after the dutch word for owl.
 #uil log "This is a log"
 
+APPLICATION_NAME="Uil Default"
+CONFIG_FILE="/etc/uil/uil.conf"
+LOG_FILE_LOCATION=""
+
+
 	#Sources the outernet Config file. 
-source /etc/outernet/outernet.conf || {
-	echo "Failed to load config file from /etc/outernet/outernet.conf">&2
+source ${CONFIG_FILE} || {
+	echo "Failed to load config file from ">&2
 	exit 1
 }
 
@@ -18,19 +23,38 @@ source /etc/outernet/outernet.conf || {
 
 case "$1" in
 
-  "log" )
-    echo "[$(get_timestamp)] - $2" >> $INSTALL_LOG_LOCATION
-  ;;
+	"log" )
+	echo "[$(get_timestamp)] - $2" >> $LOG_FILE_LOCATION
+	;;
 
   "echo" )
 	echo $2
-    echo "[$(get_timestamp)] - $2" >> $INSTALL_LOG_LOCATION
+    echo "[$(get_timestamp)] - $2" >> $LOG_FILE_LOCATION
   ;;
 
   "error" )
 	echo $2
-    echo "[$(get_timestamp)] -ERROR- $2" >> $INSTALL_LOG_LOCATION
+    echo "[$(get_timestamp)] -ERROR- $2" >> $LOG_FILE_LOCATION
   ;;
+
+	#-- System Journal Commands
+	"jinfo" )
+		systemd-cat --identifier=${} --priority=6 echo $2
+	;;
+	
+	"jwarn" )
+		systemd-cat --identifier=${} --priority=6 echo $2
+	;;
+	
+	"jcrit" )
+		systemd-cat --identifier=${} --priority=2 echo $2
+	;;
+	
+	"jdbug" )
+		systemd-cat --identifier=${} --priority=7 echo $2
+	;;
+	
+
 
   "help" )
     echo "log  -  Logs informaion to the default log."
